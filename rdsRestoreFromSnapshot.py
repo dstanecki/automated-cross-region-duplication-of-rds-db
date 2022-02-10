2e9f8f0bfdc57d137280a7c5c1d1af106289e63f
Daniel Stanecki <dstanecki@hawk.iit.edu>
2022-02-09 22:29:24 -0600

    Variables are now passed through event handler and implemented error checking
    Removed the need for altering source code to change certain variables. Added new variables (DB name, DB instance class, origin DB name) to give the user more flexibility. Added checks for errors. Also refined some of the code.

--- rdsRestoreFromSnapshot.py
+++ rdsRestoreFromSnapshot.py
@@ -1,145 +1,57 @@
-# this Code will help to schedule the restoration of an RDS database from a cross-region database snapshot
+# This code will restore an RDS database instance from the latest snapshot across regions
 # Daniel Stanecki
-# Version -- 1.0
+# Version -- 2.0
 
 import boto3
 import os
 import sys
 import time
+import json
 import array as arr
 from datetime import datetime, timezone
 from time import gmtime, strftime
 
-def restore_rds_all():
-    region=os.environ['REGION'] #region we are restoring the DB snapshot in
-    region2=os.environ['REGION2'] #region we are getting the snapshot from
-    client = boto3.client('rds', region_name=region)
-    client2 = boto3.client('rds', region_name=region2)
-    
-    response = client2.describe_db_snapshots( #returns array with two elements: DBSnapshots and Marker
-        DBInstanceIdentifier='x', #pull all automated DBSnapshots associated with database 'x' 
-        #DBSnapshotIdentifier='string',
-        SnapshotType='automated' 
-        #Filters=[
-        #    {
-        #       'Name': 'string',
-         #       'Values': [
-          #          'string',
-           #     ]
-            #},
-        #],
-        #MaxRecords=123,
-        #Marker='string',
-        #IncludeShared=True|False,
-        #IncludePublic=True|False,
-        #DbiResourceId='string'
-    )
-    
-    def myFunc(e):
-        return e['SnapshotCreateTime']
-        
-    x = response['DBSnapshots'] #x holds all DBSnapshots objects
-    x.sort(key=myFunc, reverse=True) #sort from most recent to oldest DBSnapshots
-    y = x[0] #y is a single DBSnapshots object
-    print(y['SnapshotCreateTime'])  
-    snapshotIdentifier = y['DBSnapshotIdentifier'] + '-' + os.environ['REGION2'] #cross-region snapshots have the original region appended in the identifier
-    
-    client.restore_db_instance_from_db_snapshot(
-        DBInstanceIdentifier='y',
-        DBSnapshotIdentifier=snapshotIdentifier,
-        DBInstanceClass='db.t3.micro',
-        Port=5432,
-        #AvailabilityZone='string',
-        #DBSubnetGroupName='string',
-        MultiAZ=False,
-        PubliclyAccessible=False,
-        AutoMinorVersionUpgrade=False,
-        #LicenseModel='string',
-        #DBName='',
-        Engine='postgres',
-        #Iops=123,
-        #OptionGroupName='string',
-        Tags=[
-            {
-                'Key': 'DEV-TEST',
-                'Value': 'Auto-Delete'
-            },
-        ],
-        #StorageType='string',
-        #TdeCredentialArn='string',
-        #TdeCredentialPassword='string',
-        #VpcSecurityGroupIds=[
-            #'string',
-            #],
-        #Domain='string',
-        CopyTagsToSnapshot=False,
-        #DomainIAMRoleName='string',
-        #EnableIAMDatabaseAuthentication=True|False,
-        #EnableCloudwatchLogsExports=[
-        #'string',
-        #],
-        #ProcessorFeatures=[
-        #    {
-         #       'Name': 'coreCount',
-          #      'Value': '1'
-           # },
-        #],
-        UseDefaultProcessorFeatures=True,
-        #DBParameterGroupName='string',
-        DeletionProtection=False,
-        EnableCustomerOwnedIp=False,
-        #CustomIamInstanceProfile='string',
-        #BackupTarget='string'
-    )
-
-"""    
-    client.restore_db_cluster_from_snapshot(
-        AvailabilityZones=[
-            'string',
-        ],
-        DBClusterIdentifier='string',
-        SnapshotIdentifier='string',
-        Engine='string',
-        EngineVersion='string',
-        Port=123,
-        DBSubnetGroupName='string',
-        DatabaseName='string',
-        OptionGroupName='string',
-        VpcSecurityGroupIds=[
-            'string',
-        ],
-        Tags=[
-            {
-                'Key': 'string',
-                'Value': 'string'
-            },
-        ],
-        KmsKeyId='string',
-        EnableIAMDatabaseAuthentication=True|False,
-        BacktrackWindow=123,
-        EnableCloudwatchLogsExports=[
-            'string',
-        ],
-        EngineMode='string',
-        ScalingConfiguration={
-            'MinCapacity': 123,
-            'MaxCapacity': 123,
-            'AutoPause': True|False,
-            'SecondsUntilAutoPause': 123,
-            'TimeoutAction': 'string',
-            'SecondsBeforeTimeout': 123
-        },
-        DBClusterParameterGroupName='string',
-        DeletionProtection=True|False,
-        CopyTagsToSnapshot=True|False,
-        Domain='string',
-        DomainIAMRoleName='string',
-        DBClusterInstanceClass='string',
-        StorageType='string',
-        Iops=123,
-        PubliclyAccessible=True|False
-    )
-    """
-    
 def lambda_handler(event, context):
-    restore_rds_all()
+    
+    print("request: " + json.dumps(event))
+    
+    if 'targetRegion' in event and 'crossRegion' in event and 'originDB' in event and 'newDB' in event and 'newDBClass' in event: 
+        client1 = boto3.client('rds', region_name=event['targetRegion'])
+        client2 = boto3.client('rds', region_name=event['crossRegion'])
+    else:
+        return "Event JSON body is incomplete."
+    
+    response = client2.describe_db_snapshots( # Returns array with two elements: DBSnapshots and Marker
+        DBInstanceIdentifier=event['originDB'], # Pull all automated DBSnapshots associated with the origin database in another region
+        SnapshotType='automated'
+    )
+    
+    if not response:
+        return "There are no snapshots in your selected region."
+    else:
+        snapshots = response['DBSnapshots'] # Holds all DBSnapshots objects 
+        snapshots.sort(key=lambda x: x['SnapshotCreateTime'], reverse=True) # Sort from most recent to oldest DBSnapshots
+        newestSnapshot = snapshots[0] # Single DBSnapshots object
+        # print(newestSnapshot['SnapshotCreateTime'])  
+        snapshotIdentifier = newestSnapshot['DBSnapshotIdentifier'] + '-' + event['crossRegion'] # Cross-region snapshots have the original region appended to the identifier
+    
+        client1.restore_db_instance_from_db_snapshot(
+            DBInstanceIdentifier=event['newDB'],
+            DBSnapshotIdentifier=snapshotIdentifier,
+            DBInstanceClass=event['newDBClass'],
+            Port=5432,
+            MultiAZ=False,
+            PubliclyAccessible=False,
+            AutoMinorVersionUpgrade=False,
+            Engine='postgres',
+            Tags=[
+                {
+                    'Key': 'DEV-TEST',
+                    'Value': 'Auto-Delete'
+                },
+            ],
+            CopyTagsToSnapshot=False,
+            UseDefaultProcessorFeatures=True,
+            DeletionProtection=False,
+            EnableCustomerOwnedIp=False
+        )
